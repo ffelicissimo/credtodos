08f6780df60c7601ec98abf52f3d1ad5b2f11d31
ffelicissimo@gmail.com <ffelicissimo@gmail.com>
2019-08-29 13:59:06 -0300

    Corrigindo update/upgrade docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ######################### FERNANDO FELICISSIMO ##################################
-sudo apt-get update && apt-get upgrade -y \
-             && apt-get dist-upgrade -y
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -17,5 +18,5 @@ sudo rm -rf /var/lib/apt/lists/*
 ##############################################################################
 
 ################### Executando Container #####################################
-/bin/sh -c '/usr/bin/docker run --privileged -dit --env-file=dev.env --restart always -p 80:80 --name credtodosapi --net=host ffelicissimo/credtodos:latest'
+/bin/sh -c '/usr/bin/docker run --privileged -d --env-file=dev.env --restart always -p 80:80 --name credtodosapi --net=host ffelicissimo/credtodos:latest'
 #############################################################################
